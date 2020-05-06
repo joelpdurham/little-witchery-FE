1c39a2e9c2ba5624f32e1ccefbcd5ba9c4058071
joelpdurham <joeldur@gmail.com>
2020-05-06 11:19:54 -0700

    header is spaced evenly

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.jpeg';
+// import logo from '../../assets/logo2.png';
 import styles from '../Styles/Header.css';
 
 export const Header = () => {
   return (
     <section className={styles.header}>
-      <NavLink to='/'>About</NavLink>
-      <NavLink to='/'>Book of Spells</NavLink>
-      <NavLink to='/'>Shop</NavLink>
+      <div className={styles.navLinks}>
+        <NavLink to='/'>About</NavLink>
+        <NavLink to='/'>Book of Spells</NavLink>
+        <NavLink to='/'>Shop</NavLink>
+      </div>
       <div className={styles.logo}>
         <h1>Little Witchery</h1>
         <img src={logo} />
       </div>
-      <NavLink to='/'>Blog</NavLink>
-      <NavLink to='/'>Videos</NavLink>
-      <NavLink to='/'>Contact</NavLink>
+      <div className={styles.navLinks}>
+        <NavLink to='/'>Blog</NavLink>
+        <NavLink to='/'>Videos</NavLink>
+        <NavLink to='/'>Contact</NavLink>
+      </div>
     </section>
   );
 };
