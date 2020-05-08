44ff5fac59d32926ddbcc0db1c3eded5148ccd9a
joelpdurham <joeldur@gmail.com>
2020-05-07 17:33:52 -0700

    author photo added and underscore in insta fixed

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const InstagramFeed = () => {
   return (
     <section>
-      <h2>@little-witchery</h2>
+      <h2>@little_witchery</h2>
       <p>instagram feed here</p>
     </section>
   );
