48bbbab5396eca0f64fb60bc3910ee505f393ce7
joelpdurham <joeldur@gmail.com>
2020-05-08 11:52:19 -0700

    mvp for videos hooked up, and linked throughout

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -4,6 +4,15 @@ export const Videos = () => {
   return (
     <section>
       <h1>Videos</h1>
+      <div>
+        <h2>Latest Video</h2>
+        <iframe width="600" height="325" src='https://www.youtube.com/embed/7GnsL0if_6w' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'></iframe>
+      </div>
+      <div>
+        <h2>All Videos</h2>
+      </div>
     </section>
+
+    
   );
 };
